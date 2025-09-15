7589aff64aef5c48708c09b70009278f4e6a75cd
himanshusingh109 <1learner.himanshu@gmail.com>
2025-09-15 11:24:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -81,10 +81,10 @@ const sessionOption = {
     },
 };
 
-// app.get("/", (req, res) => {
-//     res.send("Hi, I am root");
-// });
 
+app.get("/",(req,res)=>{
+    res.redirect("/listings");
+    });
 
 app.use(session(sessionOption));
 app.use(flash());
